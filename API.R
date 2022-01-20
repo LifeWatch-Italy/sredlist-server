731936189fd52b30049b01a2662a380ec8c08506
Pierfrancesco <tommasinofrancesco93@gmail.com>
2022-01-20 18:25:00 +0100

    Updated API description

--- API.R
+++ API.R
@@ -1,7 +1,7 @@
 # API.R
 
 #* @apiTitle LifeWach-sRedList-Platform API
-#* @apiDescription This is a sample server for a Red List workflow.
+#* @apiDescription This is a sample server for a sRedList workflow.
 #* @apiTOS http://example.com/terms/
 #* @apiContact list(name = "API Support", url = "http://www.example.com/support", email = "support@example.com") # nolint
 #* @apiLicense list(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html") # nolint
