3b6c4abf18792b6c3a789a1206def5d781524a1d
Victor Cazalis <victor.cazalis@laposte.net>
2024-03-11 16:10:10 +0100

    Add Shiny app to edit points manually

---
+++ Shiny_EditPts.R
@@ -0,0 +1,165 @@
+library(shiny)
+library(leaflet)
+library(mapedit)
+library(sf)
+library(htmlwidgets)
+
+source(textConnection(readLines("C:/Users/Victor/Documents/sRedList/Platform/InProgress/sredlist-server-develop/server.R")[1:73]))
+
+### Charge results from step 1b2
+# http://127.0.0.1:7839/?sci_name=Azteca_xanthochroa&user=victor.cazalis
+
+
+
+### Load functions
+source("sRLfun_ShinyEditPoints.R")
+
+
+### Specify url
+options(shiny.host = '127.0.0.1')
+options(shiny.port = 7839)
+
+
+
+
+
+### UI ----------
+ui <- fluidPage(
+  
+  tags$style(type="text/css",
+             ".shiny-notification {
+    position:fixed;
+    top: calc(25%);
+    left: calc(50%);
+  }"),
+  
+  # add map
+  sidebarLayout(
+    position="left",
+    mainPanel(
+      conditionalPanel('false', 
+                       textInput("sci_name",
+                                 "Scientific name:"),
+                       textInput("user",
+                                 "User name:")
+      ),
+      editModUI("map", height=600, width = "100%"),
+      conditionalPanel(condition = "input.sci_name != ''",
+                       column(5), column(2, actionButton('save', 'Save from Map', style="color: #fff; background-color: #009138ff; border-color: #009138ff"))
+      )
+    ),
+    sidebarPanel(
+      titlePanel("Attributes for new points"),
+      numericInput("Pts_year", label="Year", value=NA),
+      numericInput("Pts_uncert", label="Coordinate uncertainty (km)", value=NA),
+      textInput("Pts_source", label="Source", value=NA)
+    )
+  )
+)
+
+
+
+
+### Server ----------
+server <- function(input, output, session) {
+  
+  
+  
+  ### Create objects ---------
+
+  ### Create base map
+  edits <- callModule(
+    editMod,
+    leafmap = sRLMan_CreateLeaflet(),
+    id = "map",
+    record = FALSE,
+    sf = TRUE
+  )
+  
+  ### CREATE REACTIVE VALUES
+  flagsSF <- reactiveVal()
+  flags <- reactiveVal()
+  Storage_SP <- reactiveVal()
+  
+  
+  
+  ### Events ---------
+
+  ### Read the URL parameter from session$clientData$url_search
+  observe({
+    query <- parseQueryString(session$clientData$url_search)
+    updateTextInput(session, "sci_name", value = query[['sci_name']] %>% gsub("_" , " ", .) %>% gsub("%20%" , " ", .))
+    updateTextInput(session, "user", value = query[['user']])
+  })
+  
+  
+  ### Update base map with species information
+  observe({
+      req(input$sci_name)
+      req(is.null(nrow(flags()))) # Does not run if flags already loaded
+      sRL_loginfo("START - Load records", input$sci_name)
+      
+      ### Load StorageSP and flags
+      Error_mess<-tryCatch({
+        Storage_SP(sRL_StoreRead(input$sci_name,  input$user, MANDAT=1)) ; print(names(Storage_SP()))
+        flags(Storage_SP()$flags)
+      }, error=function(e){"<b>We could not find the points gathered in Step 1b of the sRedList platform. Please try again.</b>"})
+      if(Error_mess != "TRUE"){showNotification(ui=HTML(Error_mess), type="error", duration=8)}
+      
+      
+      ### Update leaflet map
+      req(flags())
+      flagsSF(sRLMan_JitterPoints(st_as_sf(flags(), coords = c("decimalLongitude", "decimalLatitude"), crs="+proj=longlat +datum=WGS84", remove=F)))
+      sRLMan_UpdateLeaflet(flagsSF(), frame=1)
+  })
+  
+  
+  
+  
+  ### Save points
+  observeEvent(input$save, {
+    
+    sRL_loginfo("START - Save manual edit records", input$sci_name)
+    
+    ### Update record flagging
+    flagsSF(sRLMan_EditPoints(edits()$finished, flagsSF(), input$Pts_year, input$Pts_uncert, input$Pts_source))
+      
+    ### Save flags
+    Storage_SPNEW <- Storage_SP()
+    Storage_SPNEW$dat_proj_saved <- sRL_SubsetGbif(flagsSF(), input$sci_name)
+    Storage_SPNEW$flags <- flagsSF() %>% as.data.frame(.) %>% .[, names(.) != "geometry"]
+    sRL_StoreSave(input$sci_name, input$user,  Storage_SPNEW)
+
+    ### Update map
+    sRLMan_UpdateLeaflet(flagsSF(), frame=1)
+
+    sRL_loginfo("END - Save manual edit records", input$sci_name)
+  })
+  
+  
+  observeEvent(input$button_valid, {
+    
+    # Identify GBIF id of the record to edit
+    GbifID <- input$button_valid %>% sub("makeinvalid_", "", .)  %>% sub("makevalid_", "", .) 
+    
+    # Isolate flagsSF and edit its "reason"
+    flagsSF_edit <- flagsSF()
+    flagsSF_edit$Reason[flagsSF_edit$gbifID==GbifID] <- ifelse(grepl("makeinvalid", input$button_valid), "Manual_invalid", NA)
+    
+    # Save changes and edit leaflet
+    flagsSF(flagsSF_edit)
+    sRLMan_UpdateLeaflet(flagsSF(), frame=0)
+    
+  })
+}
+
+
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+
---
+++ sRLfun_ShinyEditPoints.R
@@ -0,0 +1,165 @@
+### Create leaflet map
+
+sRLMan_CreateLeaflet <- function(){
+  
+  leaflet() %>%
+    addTiles()  %>%
+    addLegend(position="bottomleft", colors=c('#fde725ff', '#440154ff'), labels=c("Valid", "Not valid")) %>%
+    addMouseCoordinates() %>%
+    addScaleBar(position="bottomright") %>%
+    leaflet.extras::addSearchOSM(options = searchOptions(collapsed = FALSE, position="topright")) %>%
+    addDrawToolbar(
+      polylineOptions = FALSE,
+      circleOptions = FALSE,
+      markerOptions = drawMarkerOptions(repeatMode = TRUE),
+      circleMarkerOptions = FALSE,
+      editOptions=editToolbarOptions(edit=TRUE, remove=TRUE)
+    ) %>%
+    onRender("function(el, x){
+               var map = this;
+               var polygons = [];
+               map.on('layeradd', function(e) {
+                 polygons.push(e.layer);
+               });
+               $('#save').on('click', function() {
+                 var features = [];
+                 for(var polygon of polygons) {
+                   if(polygon.feature) {
+                     features.push(polygon);
+                     map.removeLayer(polygon);
+                   }
+                 }
+                 polygons = [];
+                 var group = L.layerGroup(features);
+                 var collection = group.toGeoJSON(false);
+                 var n = collection.features.length;
+                 var i = 0;
+                 var interval = setInterval(function() {
+                    if(i === n) {
+                      clearInterval(interval);
+                    } else {
+                      var f = collection.features[i];
+                      var deleted = {type: 'FeatureCollection', features: [f]};
+                      Shiny.setInputValue(el.id + '_draw_deleted_features', deleted);
+                      i = i + 1;
+                    }
+                 });
+               });
+             }")
+  
+} 
+
+
+
+
+sRLMan_UpdateLeaflet <- function(flagsSF, frame){
+  
+  # Update Popup
+  flagsSF <- sRL_PopRecords(flagsSF)
+  
+  # Update map
+  leafletProxy("map-map") %>% 
+    removeMarker(layerId = paste0("CircleFlags", 1:nrow(flagsSF))) %>%
+    addCircleMarkers(lng=flagsSF$Lon_jitt,
+                     lat=flagsSF$Lat_jitt,
+                     color=ifelse(is.na(flagsSF$Reason)==T, "#fde725ff", "#440154ff"),
+                     layerId=paste0("CircleFlags", 1:nrow(flagsSF)),
+                     fillOpacity=0.5,
+                     stroke=F,
+                     popup=paste0(
+                       flagsSF$PopText, '<br>', 
+                       shinyInputPts(actionButton, flagsSF$gbifID, Valid = is.na(flagsSF$Reason))
+                     ),
+                     radius=8)
+  
+  # Update framing if frame==1
+  if(frame==1){
+    # Get extent
+    EXT <- ext(flagsSF)*1.1 %>% as.numeric()
+    
+    # Update map
+    leafletProxy("map-map") %>% 
+      fitBounds(lng1 = as.numeric(EXT[1]), lng2 = as.numeric(EXT[2]), lat1 = as.numeric(EXT[3]), lat2 = as.numeric(EXT[4]))
+  }
+  
+}
+
+
+
+
+sRLMan_EditPoints <- function(EditsGeom, flagsSF, Pts_year, Pts_uncert, Pts_source){
+  
+  ### Remove points overlapping with polygon
+  if("rectangle" %in% EditsGeom$feature_type | "polygon" %in% EditsGeom$feature_type){
+    Points_to_rm<-st_intersection(flagsSF, EditsGeom[EditsGeom$feature_type %in% c("polygon", "rectangle"),])
+    flagsSF$Manu_removed<-flagsSF$gbifID %in% Points_to_rm$gbifID
+    flagsSF$Reason[flagsSF$Manu_removed==T]<-"Manually_removed"
+    flagsSF$Manu_removed<-NULL
+  }
+  
+  ### Add points added by user
+  if("marker" %in% EditsGeom$feature_type){
+    
+    ## Check if we already have manual points to adapt the numerotation
+    NUM <- flagsSF$gbifID %>% .[grepl("Manual_", .)] %>% sub("Manual_", "", .) %>% as.numeric(.) %>% max(c(., 0), na.rm=T)
+    print(NUM)
+    
+    ## Populate Points to add
+    Points_to_add<-EditsGeom[EditsGeom$feature_type=="marker",]
+    Points_to_add$gbifID<-paste0("Manual_", (NUM+1):(NUM+nrow(Points_to_add)))
+    Points_to_add$Reason<-NA
+    Points_to_add$Source_type<-"Manually added"
+    Points_to_add$decimalLongitude<-st_coordinates(Points_to_add)[,1]
+    Points_to_add$decimalLatitude<-st_coordinates(Points_to_add)[,2]
+    Points_to_add$leaflet_id<-Points_to_add$feature_type<-NULL
+    Points_to_add$Lon_jitt <- Points_to_add$decimalLongitude
+    Points_to_add$Lat_jitt <- Points_to_add$decimalLatitude
+    
+    Points_to_add$year <- Pts_year
+    Points_to_add$coordinateUncertaintyInMeters <- Pts_uncert*1000
+    Points_to_add$source <- Pts_source
+    
+    ## Merge
+    flagsMerged <- rbind.fill(as.data.frame(flagsSF), as.data.frame(Points_to_add))
+    flagsMerged$geometry <- NULL
+    flagsSF <- st_as_sf(flagsMerged, coords = c("decimalLongitude", "decimalLatitude"), crs="+proj=longlat +datum=WGS84", remove=F)
+    flagsSF <- sRL_PopRecords(flagsSF)
+  }
+  
+  return(flagsSF)
+}
+
+
+sRLMan_JitterPoints <- function(flagsSF){
+  
+  # Jitter coordinates if they are not unique
+  print("JITTER")
+  flags_jitt <- flagsSF %>% st_jitter(., amount=0.0002) %>% st_coordinates(.)
+  flagsSF$Coords_merged <- paste0(flagsSF$decimalLongitude, flagsSF$decimalLatitude) 
+  flagsSF$unique <- !(flagsSF$Coords_merged %in% flagsSF$Coords_merged[duplicated(flagsSF$Coords_merged)])
+  flagsSF$Lon_jitt <- flagsSF$decimalLongitude ; flagsSF$Lat_jitt <- flagsSF$decimalLatitude
+  flagsSF$Lon_jitt[flagsSF$unique==F]<-flags_jitt[flagsSF$unique==F, 1]
+  flagsSF$Lat_jitt[flagsSF$unique==F]<-flags_jitt[flagsSF$unique==F, 2]
+  
+  return(flagsSF)
+  
+}
+
+
+shinyInputPts <- function(FUN, SPs, Valid) {
+  inputs <- character(length(SPs))
+  for (i in 1:length(SPs)) {
+    inputs[i] <- as.character(
+      FUN(
+        paste0(
+          ifelse(Valid[i]==T, "makeinvalid_", "makevalid_"), 
+          sub(" ", "_", SPs[i])
+        ),
+        label=ifelse(Valid[i]==T, "Make Invalid", "Make Valid"), 
+        style=paste0("color: ", ifelse(Valid[i]==T, "#fff", "#000"), "; background-color: ", ifelse(Valid[i]==T, "#440154ff", "#fde725ff"), "; border-color: ", ifelse(Valid[i]==T, "#440154ff", "#fde725ff")), 
+        onclick="Shiny.onInputChange( \"button_valid\" , this.id)"
+      )
+    )
+  }
+  inputs
+}
