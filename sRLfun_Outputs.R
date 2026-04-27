ab59094b48e6668a7e54bacdc6289f2bb8e4ffbe
Victor Cazalis <victor.cazalis@conservara.fr>
2026-04-27 11:03:22 +0200

    Combine maps in HTML in Merge ZIP functionality

--- sRLfun_Outputs.R
+++ sRLfun_Outputs.R
@@ -703,6 +703,25 @@ sRL_CriteriaCalculator <- function(allfields){
 
 
 
+### Combine different distribution maps into a single HTML document (eg for workshops etc)
+sRL_CombineMapsHTML <- function(dist, pts, Zip_Path){
+  
+  ### Transform data
+  dist <- dist %>% st_transform(., st_crs(4326)) %>% sRL_ColourDistrib(.)
+  if(is.null(pts)==F){pts <- pts %>% sRL_ColourDistrib(.)}
+  
+  library(leaflet)
+  library(htmltools)
+  
+  ### Call RMD
+  render(
+    "sRL_markdown_scripts/sRL_Compile_distributions_in_pdf.Rmd",
+    output_file="Compiled_maps_sRedList.html",
+    output_dir=Zip_Path
+  )
+  
+}
+
 
 
 
